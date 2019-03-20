7e5ecf4f142a3894f3921029f54fdee6f848e29a
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-03-20 10:20:05 -0300

    :sparkles: Add humanoid env

---
+++ humanoid/humanoid.py
@@ -0,0 +1,70 @@
+import gym
+import os
+from baselines.ppo1 import mlp_policy, pposgd_simple
+from humanoid_env import HumanoidEnv
+import baselines.common.tf_util as U
+import tensorflow as tf  # pylint: ignore-module
+
+## Constants
+NUM_TIMESTEPS = 300_000
+MODEL_PATH = './saver/humanoid.tf'
+ 
+# Copiado de https://github.com/openai/baselines/blob/master/baselines/ppo1/run_humanoid.py:40~45
+class RewScale(gym.RewardWrapper):
+    def __init__(self, env, scale):
+        gym.RewardWrapper.__init__(self, env)
+        self.scale = scale
+
+    def reward(self, r):
+        return r * self.scale
+
+def callback(lcl, glb):
+    # stop training if reward exceeds 199
+    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    return is_solved
+
+def save_state(fname):
+    dirname = os.path.dirname(fname)
+    if any(dirname):
+        os.makedirs(dirname, exist_ok=True)
+    saver = tf.train.Saver()
+    saver.save(tf.get_default_session(), fname)
+
+# Not used now
+def load_state(fname):
+    saver = tf.train.Saver()
+    saver.restore(tf.get_default_session(), fname)
+
+# Wrapper for mlp_policy.MlpPolicy
+def policy_fn(name, ob_space, ac_space):
+    return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+        hid_size=64, num_hid_layers=2)
+ 
+def main():
+    # Copiado de https://github.com/openai/baselines/blob/master/baselines/ppo1/run_humanoid.py
+    U.make_session(num_cpu=1).__enter__()
+
+    # create the environment
+    env = HumanoidEnv()
+    env = RewScale(env, 0.8)
+
+    # train the agent on the environment
+    pposgd_simple.learn(env, policy_fn,
+        max_timesteps=NUM_TIMESTEPS,
+        timesteps_per_actorbatch=2048,
+        clip_param=0.1, entcoeff=0.0,
+        optim_epochs=10,
+        optim_stepsize=1e-4,
+        optim_batchsize=64,
+        gamma=0.99,
+        lam=0.95,
+        schedule='constant',
+    )
+    env.close()
+    
+    # Save trained model
+    save_state(MODEL_PATH)
+ 
+if __name__ == '__main__':
+    main()
+
---
+++ humanoid/humanoid_env.py
@@ -0,0 +1,158 @@
+import gym
+import math
+import time
+import numpy as np
+import pybullet as p
+import pybullet_data
+import math
+import os
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+# Enums
+X, Y, Z = range(3)
+
+# Constants
+FORCE_FACTOR = 0.082
+MOTOR_NAMES =  ["abdomen_z", "abdomen_y", "abdomen_x", \
+                    "right_hip_x", "right_hip_z", "right_hip_y", "right_knee", \
+                    "left_hip_x", "left_hip_z", "left_hip_y", "left_knee", \
+                    "right_shoulder1", "right_shoulder2", "right_elbow", \
+                    "left_shoulder1", "left_shoulder2", "left_elbow"]
+MOTOR_POWERS = [100, 100, 100, 100, 100, 300, 200, 100, 100, 300, 200, 25, 25, 25, 25, 25, 25]
+ 
+class HumanoidEnv(gym.Env):
+  metadata = {
+    'render.modes': ['human', 'rgb_array'],
+    'video.frames_per_second' : 50
+  }
+ 
+  def __init__(self, envStepCounterLimit = 2_048):
+    """
+      HumanoidEnv initialization method, which creates the self.action_space and the self.observation_space variables,
+      as well as connect to the pyBullet physics client
+    """
+    self.action_space = spaces.Box(-2, +2, (17, ))          # Actions can be exerced in the [-2, 2] range
+    self.observation_space = spaces.Box(-10, +10, (43, ))   # Observations will be taken in a [-10, 10] range
+
+    self._observation = []                                  # Array which stores the observations
+    self._envStepCounterLimit = envStepCounterLimit         # Maximum frames which the env will try to run, before it's done (default 10_000)
+
+    self.physicsClient = p.connect(p.GUI)                   # Create the pyBullet physics client
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Used to load some default models from pyBullet (such as the field)
+
+    self._seed()                                            # Configure the seed (random number generator)
+ 
+  def step(self, action):
+    """
+      Method which is called whenever a new action should be taken by the HumanoidEnv
+    """
+    self._assign_throttle(action)
+    p.stepSimulation()
+    self._observation = self._compute_observation()
+    reward = self._compute_reward()
+    done = self._compute_done()
+ 
+    self._envStepCounter += 1
+    return np.array(self._observation), reward, done, {}
+
+  def reset(self):
+    self.initial_position = None
+    self._actions = np.zeros((17,))
+    self._envStepCounter = 0
+  
+    p.resetSimulation()
+    print("[RESET]")
+    p.setGravity(0, 0, -10) # m/s^2
+    p.setTimeStep(0.01) # sec
+  
+    p.loadURDF("plane.urdf")
+
+    cubeStartPos, cubeStartOrientation = [0, 0, 0.001], p.getQuaternionFromEuler([0, 0, 0])
+    self.botId = p.loadMJCF(os.path.join(os.path.abspath(os.path.dirname(__file__)), "humanoid.xml"),
+                            flags = p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS)[0]
+    p.resetBasePositionAndOrientation(self.botId, cubeStartPos, cubeStartOrientation)
+
+    self.ordered_joints = []
+    self.ordered_joint_indices = []
+    joint_dict = {}
+    for j in range(p.getNumJoints(self.botId)):
+
+        # Grab JointInformation
+        info = p.getJointInfo(self.botId, j)
+        link_name = info[12].decode("ascii")
+
+        # Configure foots (used for collision with the terrain)
+        if link_name=="left_foot": self.left_foot_index = j
+        if link_name=="right_foot": self.right_foot_index = j
+        self.ordered_joint_indices.append(j)
+
+        if info[2] == p.JOINT_REVOLUTE:
+            joint_dict[info[1].decode("ascii")] = j
+            self.ordered_joints.append( (j, info[8], info[9]) ) # (Index, Joint Lower Limit, Joint Higher Limit)
+            
+            # Reset forces
+            p.setJointMotorControl2(self.botId, j, controlMode=p.VELOCITY_CONTROL, force=0)
+
+    self._motors_power = MOTOR_POWERS
+    self._motors_index = [joint_dict[name] for name in MOTOR_NAMES]
+  
+    self._observation = self._compute_observation()
+    return np.array(self._observation)
+
+  # PyBullet handles this
+  def _render(self, mode='human', close=False):
+    pass
+  
+  def _seed(self, seed=None):
+    self.np_random, seed = seeding.np_random(seed)
+    return [seed]  
+
+
+  ####
+  ## Helpers ##
+  def _assign_throttle(self, actions):
+      actions = [self._motors_power[idx] * action * FORCE_FACTOR for idx, action in enumerate(actions)]
+      self._actions = np.clip(self._actions + actions, -5, 5)
+      p.setJointMotorControlArray(self.botId, self._motors_index, controlMode=p.TORQUE_CONTROL, forces=self._actions)
+
+  def _compute_observation(self):
+    jointStates = p.getJointStates(self.botId, self.ordered_joint_indices)
+    joints = np.array([self._current_relative_position(jointStates, *jtuple) for jtuple in self.ordered_joints]).flatten()
+    
+    rcont = p.getContactPoints(self.botId, -1, self.right_foot_index, -1)
+    lcont = p.getContactPoints(self.botId, -1, self.left_foot_index, -1)
+    feet_contact = np.array([len(rcont) > 0, len(lcont) > 0])
+
+    body_xyz, body_quaternion = p.getBasePositionAndOrientation(self.botId)
+    (qx, qy, qz) = p.getEulerFromQuaternion(body_quaternion)
+    if self.initial_position == None:
+        self.initial_position = body_xyz
+
+    (vx, vy, vz), _ = p.getBaseVelocity(self.botId)        
+    extra_info = np.array([body_xyz[Z] - self.initial_position[Z], 0.1*vx, 0.1*vy, 0.1*vz, qx, qy, qz])
+
+    return np.clip(np.concatenate([joints] + [feet_contact] + [extra_info]), -10, +10)
+
+  def _current_relative_position(self, jointStates, j, lower, upper):
+    state  = jointStates[j]
+    pos, vel = state[0], state[1]
+    pos_mid = 0.5 * (lower + upper)
+
+    return (2 * (pos - pos_mid) / (upper - lower), 0.1 * vel)
+
+
+
+  def _compute_reward(self):
+    cubePos, _ = p.getBasePositionAndOrientation(self.botId)
+    linearVel, _ = p.getBaseVelocity(self.botId)
+
+    self._reward = linearVel[X] - (10 + (cubePos[X] * 10 / (self._envStepCounter + 1)))
+    return self._reward
+
+  def _compute_done(self):
+    cubePos, _ = p.getBasePositionAndOrientation(self.botId)
+    done = self._envStepCounter >= self._envStepCounterLimit or cubePos[Z] < -0.1 or cubePos[Z] > 3 # Enough steps or below ground, or too high
+    if done:
+      print(f"[Final Reward] {self._reward} - LinearVel[X]: {p.getBaseVelocity(self.botId)[0][X]}")
+    return done 
