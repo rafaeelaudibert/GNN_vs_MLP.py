e89ca140f9f1e4c6eb8a7e9829ad06a39e389fd9
RafaAudibert-LS01 <rafaeelaudibert@gmail.com>
2019-03-27 09:59:26 -0300

    :sparkles: Add testing feature

--- humanoid/humanoid.py
+++ humanoid/humanoid.py
@@ -1,15 +1,17 @@
 import gym
 import os
+import argparse
 from baselines.ppo1 import mlp_policy, pposgd_simple
 from humanoid_env import HumanoidEnv
 import baselines.common.tf_util as U
+import pybullet as p
 import tensorflow as tf  # pylint: ignore-module
 
 ## Constants
 NUM_TIMESTEPS = 50_000_000
 MODEL_PATH = './saver/humanoid.tf'
  
-# Copiado de https://github.com/openai/baselines/blob/master/baselines/ppo1/run_humanoid.py:40~45
+# Scales the reward according to a parameter
 class RewScale(gym.RewardWrapper):
     def __init__(self, env, scale):
         gym.RewardWrapper.__init__(self, env)
@@ -18,11 +20,7 @@ class RewScale(gym.RewardWrapper):
     def reward(self, r):
         return r * self.scale
 
-def callback(lcl, glb):
-    # stop training if reward exceeds 199
-    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
-    return is_solved
-
+# Function which saves the actual tensorflow graph state
 def save_state(fname):
     dirname = os.path.dirname(fname)
     if any(dirname):
@@ -30,7 +28,7 @@ def save_state(fname):
     saver = tf.train.Saver()
     saver.save(tf.get_default_session(), fname)
 
-# Not used now
+# Function which loads a file to the tensorflow graph
 def load_state(fname):
     saver = tf.train.Saver()
     saver.restore(tf.get_default_session(), fname)
@@ -41,29 +39,62 @@ def policy_fn(name, ob_space, ac_space):
         hid_size=256, num_hid_layers=2)
  
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test", help="Runs the code in the testing mode", action="store_true")
+    args = parser.parse_args()
+
     # Copiado de https://github.com/openai/baselines/blob/master/baselines/ppo1/run_humanoid.py
     U.make_session(num_cpu=1).__enter__()
 
-    # create the environment
-    env = HumanoidEnv()
-    env = RewScale(env, 0.8)
-
-    # train the agent on the environment
-    pposgd_simple.learn(env, policy_fn,
-        max_timesteps=NUM_TIMESTEPS,
-        timesteps_per_actorbatch=2048,
-        clip_param=0.1, entcoeff=0.0,
-        optim_epochs=10,
-        optim_stepsize=1e-4,
-        optim_batchsize=64,
-        gamma=0.99,
-        lam=0.95,
-        schedule='constant',
-    )
-    env.close()
+    if not args.test:
+	 # create the environment
+        env = HumanoidEnv()
+        env = RewScale(env, 0.8)
+        
+        # train the agent on the environment
+        pposgd_simple.learn(env, policy_fn,
+            max_timesteps=NUM_TIMESTEPS,
+            timesteps_per_actorbatch=1024,
+            clip_param=0.1, entcoeff=0.0,
+            optim_epochs=10,
+            optim_stepsize=1e-4,
+            optim_batchsize=64,
+            gamma=0.99,
+            lam=0.95,
+            schedule='constant',
+        )
+        env.close()
     
-    # Save trained model
-    save_state(MODEL_PATH)
+        # Save trained model
+        save_state(MODEL_PATH)
+    else:
+        # create the environment
+        env = HumanoidEnv(connection=p.GUI)
+        env = RewScale(env, 0.8)
+
+        # run the agent only one step
+        pi = pposgd_simple.learn(env, policy_fn,
+            max_timesteps=1,
+            timesteps_per_actorbatch=128,
+            clip_param=0.1, entcoeff=0.0,
+            optim_epochs=10,
+            optim_stepsize=1e-4,
+            optim_batchsize=64,
+            gamma=0.99,
+            lam=0.95,
+            schedule='constant',
+        )
+        env.close()
+	
+        # Load trained model
+        load_state(MODEL_PATH)
+    
+        ob = env.reset()
+        while True:
+            action = pi.act(stochastic=False, ob=ob)[0]
+            ob, _, done, _ = env.step(action)
+            if done:
+                ob = env.reset()
  
 if __name__ == '__main__':
     main()
